37618b7720d50a9f47d6e432660b4581dc88d892
Dearbhla <dearbhlalane@gmail.com>
2026-01-29 14:52:35 +0000

    yes

--- Sem2/Assets/Scripts/Animation.cs
+++ Sem2/Assets/Scripts/Animation.cs
@@ -1,16 +1,42 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class Animation : MonoBehaviour
 {
+    Animator animator;
+
+    float verticalInput;
+    float horizontalInput;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+    }
+
+    void FixedUpdate()
+    {
+        animator.SetFloat ("vAxisInput", verticalInput);
+        animator.SetFloat ("hAxisInput", horizontalInput);
         
+        if (Input.GetKey (KeyCode.Z))
+        {
+            animator.SetBool ("runBool", true);
+            UnityEngine.Debug.Log("Run");
+        }
+        else
+        {
+            animator.SetBool ("runBool", false);
+            UnityEngine.Debug.Log(" No Run");
+            
+        }
     }
 }
